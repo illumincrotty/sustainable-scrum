e759e153a9238dc78938b0b6e32ca0ee13a95be1
Emma Steinman <emmasteinman@Emmas-MacBook-Pro-2.local>
2020-04-10 21:30:55 -0400

    working graphs with fake data 4/10

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,25 +2,243 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-
+import Chart from "chart.js";
 
 function Title(){
   return (
-    <div> Welcome to Denison Sustainability Dashboard! </div>
+    <div className="header"> Welcome to Denison Sustainability Dashboard! </div>
+  );
+}
+function getRandomArray(numItems) {
+  // Create random array of objects
+  let names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let data = [];
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      label: names[i],
+      value: Math.round(20 + 80 * Math.random())
+    });
+  }
+  return data;
+}
+
+function getRandomDateArray(numItems) {
+  // Create random array of objects (with date)
+  let data = [];
+  let baseTime = new Date('2018-05-01T00:00:00').getTime();
+  let dayMs = 24 * 60 * 60 * 1000;
+  for(var i = 0; i < numItems; i++) {
+    data.push({
+      time: new Date(baseTime + i * dayMs),
+      value: Math.round(20 + 80 * Math.random())
+    });
+  }
+  return data;
+}
+function getData() {
+  let data = [];
+
+  data.push({
+    title: 'Visits',
+    data: getRandomDateArray(150)
+  });
+
+  data.push({
+    title: 'Categories',
+    data: getRandomArray(20)
+  });
+
+  data.push({
+    title: 'Categories',
+    data: getRandomArray(10)
+  });
+
+  data.push({
+    title: 'Data 4',
+    data: getRandomArray(6)
+  });
+
+  return data;
+}
+
+function getFeeds() {
+  let feeds = [];
+
+  feeds.push({
+    title: 'Visits',
+    data: getRandomDateArray(150)
+  });
+
+  feeds.push({
+    title: 'Categories',
+    data: getRandomArray(20)
+  });
+
+  feeds.push({
+    title: 'Categories',
+    data: getRandomArray(10)
+  });
+
+  feeds.push({
+    title: 'Data 4',
+    data: getRandomArray(6)
+  });
+
+  return feeds;
+}
+
+class BarChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.chartRef = React.createRef();
+  }
+
+  componentDidMount() {
+  this.myChart = new Chart(this.chartRef.current, {
+    type: 'bar',
+    options: {
+      scales:{
+        yAxes:[
+          {ticks:{
+            min:0,
+            max:100
+          }}
+        ]
+      }
+    },
+    data: {
+      labels: this.props.data.map(d => d.label),
+      datasets: [{
+        label: this.props.title,
+        data: this.props.data.map(d => d.value),
+        backgroundColor: this.props.color
+      }]
+  }
+});
+
+}
+componentDidUpdate(){
+  this.myChart.data.labels = this.props.data.map(d => d.label);
+  this.myChart.data.datasets[0].data = this.props.data.map(d => d.value);
+  this.myChart.update();
+}
+render(){
+  return (
+    <canvas ref={this.chartRef}/>
   );
 }
 
-class Words extends React.Component {
+}
+
+class LineChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.chartRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    this.myChart.data.labels = this.props.data.map(d => d.time);
+    this.myChart.data.datasets[0].data = this.props.data.map(d => d.value);
+    this.myChart.update();
+  }
+
+  componentDidMount() {
+    this.myChart = new Chart(this.chartRef.current, {
+      type: 'line',
+      options: {
+        scales: {
+          xAxes: [
+            {
+              type: 'time',
+              time: {
+                unit: 'week'
+              }
+            }
+          ],
+          yAxes: [
+            {
+              ticks: {
+                min: 0
+              }
+            }
+          ]
+        }
+      },
+      data: {
+        labels: this.props.data.map(d => d.time),
+        datasets: [{
+          label: this.props.title,
+          data: this.props.data.map(d => d.value),
+          fill: 'none',
+          backgroundColor: this.props.color,
+          pointRadius: 2,
+          borderColor: this.props.color,
+          borderWidth: 1,
+          lineTension: 0
+        }]
+      }
+    });
+  }
+  render() {
+    return <canvas ref={this.chartRef} />;
+  }
+}
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feeds : getFeeds()
+    };
+  }
+
+  componentDidMount() {
+    window.setInterval(() => {
+      this.setState({
+        feeds: getFeeds()
+      })
+    }, 5000)
+  }
+
+  render() {
+    return (
+      <div className="Dashboard">
+      <LineChart
+          data={this.state.feeds[0].data}
+          title={this.state.feeds[0].title}
+          color="#3E517A"
+        />
+      <BarChart className="Dashboard"
+      data = {this.state.feeds[1].data}
+      title = {this.state.feeds[1].title}
+      color="#70CAD1"
+      />
+      </div>
+    );
+  }
+}
+
+class Graph extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      months : 12,
+      water : true,
+      electric : false,
+      solar : false,
+
+    };
+  };
   render() {
     return(
-      <div>{[1,2,3]} </div>
+      <div className="body2">{[1,2,3]} </div>
     );
   }
 }
 
 function Footer() {
   return (
-    <div> For more information visit www.denison.edu/sustainability </div>
+    <div className="footer"> For more information visit www.denison.edu/sustainability </div>
   );
 }
 
@@ -29,7 +247,7 @@ class Page extends React.Component{
     return(
       <div>
       <Title/>
-      <Words data={[1,2,3]}/>
+      <Dashboard className="body2"/>
       <Footer/>
       </div>
     );
